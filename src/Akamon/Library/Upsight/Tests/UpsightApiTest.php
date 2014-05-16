a644fdab095b3d61557af4baa2fc92ccbd4a117e
Jordi Llonch <llonch.jordi@gmail.com>
2014-05-16 15:27:20 +0200

    add debug mode

--- src/Akamon/Library/Upsight/Tests/UpsightApiTest.php
+++ src/Akamon/Library/Upsight/Tests/UpsightApiTest.php
@@ -3,14 +3,54 @@
 namespace Akamon\Library\Upsight\Tests;
 
 use Akamon\Library\Upsight\UpsightApi;
+use PHPUnit_Framework_TestCase;
 
-class UpsightApiTest extends \PHPUnit_Framework_TestCase
+class UpsightApiTest extends PHPUnit_Framework_TestCase
 {
-    public function test_api_class_can_be_loaded()
+    const API_KEY = 'some_api_key';
+
+    /** @test */
+    public function api_class_can_be_loaded()
     {
-        $api = new UpsightApi('some_api_key', ['useTestServer' => true, 'validateParams' => true]);
+        $api = new UpsightApi(self::API_KEY, ['useTestServer' => true, 'validateParams' => true]);
 
         $this->assertEquals('Akamon\Library\Upsight\UpsightApi', get_class($api));
+    }
+
+    /** @test */
+    public function send_track_event_in_debug_mode()
+    {
+        $api = new UpsightApi(self::API_KEY, ['debug' => true, 'validateParams' => true]);
+
+        $this->sendAndAssert($api);
+    }
+
+    /** @test */
+    public function send_track_event_to_prod_server()
+    {
+        $api = new UpsightApi(self::API_KEY, ['useTestServer' => false, 'validateParams' => true]);
+
+        $this->sendAndAssert($api);
+    }
+
+    /** @test */
+    public function send_track_event_to_test_server()
+    {
+        $api = new UpsightApi(self::API_KEY, ['useTestServer' => true, 'validateParams' => true]);
+
+        $this->sendAndAssert($api);
+    }
+
+    private function sendAndAssert(UpsightApi $api)
+    {
+        $optionalParameters = [
+            'value'    => 10,
+            'subtype1' => 'abc',
+            'subtype2' => 'def',
+            'subtype3' => 'ghi',
+            'data'     => '{a: 1}',
+        ];
+        $this->assertTrue($api->trackEvent(1234, 'foo', $optionalParameters));
     }
 }
  
